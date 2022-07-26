e74ec1ab33d9e264a45f95008a37b234615d069d
wuwangcheng <wuwangcheng@58.com>
2022-07-26 18:00:41 +0800

    feat: 分支dev1.1修改提交

--- app.js
+++ app.js
@@ -1,130 +1,37 @@
-const simpleGit = require('simple-git');
-
-const git = simpleGit('./');
-
-let PREV_BRANCH_NAME = '';
-
 /**
- * ============
+ * ========================
+ * 2022-07-26
  * 1.检出一个分支rollback_dev分支(可能已经存在)
  * 2.合并master分支代码到rollback_dev
  * 3.charry_up pageConfig.js文件到rollback_dev
  * 4.提交rollback_dev分支到远程
  * 5.切换到原来开发分支
  * 6.调用arthur自动构建
- * ============
+ * ========================
 */
+const simpleGit = require('simple-git');
 
+const git = simpleGit('./');
 
-/**
- * @desc git status
-*/
-async function getStatus() {
-    try {
-        return await git.status();
-    } catch (err) {
-        console.log('git_status报错:', err);
-    }
-}
-
-
-/**
- * @desc git add
-*/
-async function getAdd() {
-    try {
-        await git.add('./*');
-    } catch (err) {
-        console.log('git_add报错:', err);
-    }
-}
-
-/**
- * @desc git commit
-*/
-async function getCommit() {
-    try {
-        await git.commit('feat: commit roolback branch...');
-    } catch (err) {
-        console.log('git_commit报错:', err);
-    }
-}
-
-
-/**
- * @desc git pull
-*/
-async function getPull() {
-    try {
-        await git.pull();
-    } catch (err) {
-        console.log('git_pull报错:', err);
-    }
-}
-
-/**
- * @desc 推送当前分支修改
-*/
-async function getPushCurrBranch() {
-    try {
-        const bname = await git.raw(['branch']);
-
-        await git.push('origin', bname);
-    } catch (err) {
-        console.log('推送当前分支报错:', err);
-    }
-}
-
-
-
-
-/**
- * @desc 获取之前的分支名
-*/
-async function getPrevBranch() {
-    try {
-        return await git.raw(['branch']);
-    } catch (err) {
-        console.log('git_branch报错:', err);
-    }
-}
-
-
-async function actionStepOne() {
-    const status = await getStatus();
-
-    const { files, current } = status;
+const actionInit = async () => {
+    const { files, current } = await git.status();
 
     if (files.length) {
         await git.add('./*');
 
-        await git.commit(`feat: 推送当前${current}分支修改`);
+        await git.commit(`feat: 分支${current}修改提交`);
 
         await git.pull('origin', current);
 
-        const push = await git.push('origin', current);
-
-        console.log('---push---', push);
-    }
-
-}
-
-
-/**
- * @desc 开始执行
-*/
-async function run() {
-    try {
-        //PREV_BRANCH_NAME = await git.raw(['rev-parse', '--abbrev-ref', 'HEAD']);
-        //console.log('res:', PREV_BRANCH_NAME);
-
-        actionStepOne();
-
-        //await git.pull('origin', 'dev1.1');
-    } catch (err) {
-        console.log('函数run报错:', err);
+        await git.push('origin', current);
+        console.log('---log1---')
+        await git.checkout(['-b', 'rollback_dev', 'master']);
+    } else {
+        console.log('---log2---')
+        await git.checkout(['-b', 'rollback_dev', 'master']);
     }
 }
 
+actionInit();
 
-run();
+
--- rollback.js
+++ rollback.js
@@ -140,3 +140,166 @@ init();
 //         console.log('getRepInfo:', err);
 //     }
 // }
+
+
+
+
+
+//============================================
+
+
+/**
+ * @desc git status
+*/
+async function getStatus() {
+    try {
+        return await git.status();
+    } catch (err) {
+        console.log('git_status报错:', err);
+    }
+}
+
+
+/**
+ * @desc git add
+*/
+async function getAdd() {
+    try {
+        await git.add('./*');
+    } catch (err) {
+        console.log('git_add报错:', err);
+    }
+}
+
+/**
+ * @desc git commit
+*/
+async function getCommit() {
+    try {
+        await git.commit('feat: commit roolback branch...');
+    } catch (err) {
+        console.log('git_commit报错:', err);
+    }
+}
+
+
+/**
+ * @desc git pull
+*/
+async function getPull() {
+    try {
+        await git.pull();
+    } catch (err) {
+        console.log('git_pull报错:', err);
+    }
+}
+
+/**
+ * @desc 推送当前分支修改
+*/
+async function getPushCurrBranch() {
+    try {
+        const bname = await git.raw(['branch']);
+
+        await git.push('origin', bname);
+    } catch (err) {
+        console.log('推送当前分支报错:', err);
+    }
+}
+
+
+
+
+/**
+ * @desc 获取之前的分支名
+*/
+async function getPrevBranch() {
+    try {
+        return await git.raw(['branch']);
+    } catch (err) {
+        console.log('git_branch报错:', err);
+    }
+}
+
+async function actionCommit() {
+    try {
+        const status = await getStatus();
+
+        const { files, current } = status;
+
+
+    } catch (err) {
+        console.log('提交操作报错:', err);
+    }
+}
+
+
+async function actionStepOne() {
+    try {
+        const status = await getStatus();
+
+        const { files, current } = status;
+
+        if (files.length) {
+            await git.add('./*');
+
+            await git.commit(`feat: 分支${current}修改提交`);
+
+            await git.pull('origin', current);
+
+            const push = await git.push('origin', current);
+
+            console.log('---push---', push);
+            if (push.update && Object.keys(push.update).length) {
+                actionStepTwo();
+            }
+        } else {
+            actionStepTwo();
+        }
+    } catch (err) {
+        console.log('提交当前分支报错:', err);
+    }
+}
+
+async function actionStepTwo() {
+    try {
+        const status = await getStatus();
+
+        const { current } = status;
+
+        await git.checkout(['-b', 'rollback_dev']);
+
+        await git.mergeFromTo('origin/master', 'rollback_dev');
+
+        await git.add('./*');
+
+        await git.commit(`feat: 分支${current}修改提交`);
+
+        await git.pull('origin', current);
+
+        await git.push('origin', current);
+
+    } catch (err) {
+        console.log('获取回滚分支报错:', err);
+    }
+}
+
+
+/**
+ * @desc 开始执行
+*/
+async function run() {
+    try {
+        //PREV_BRANCH_NAME = await git.raw(['rev-parse', '--abbrev-ref', 'HEAD']);
+        //console.log('res:', PREV_BRANCH_NAME);
+
+        actionStepOne();
+
+        //await git.pull('origin', 'dev1.1');
+    } catch (err) {
+        console.log('函数run报错:', err);
+    }
+}
+
+
+run();
