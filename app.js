a8e81f5490fd1d1f16c904c236237385542509cb
wuwangcheng <wuwangcheng@58.com>
2022-07-26 19:45:11 +0800

    feat: 分支dev1.0修改提交

---
+++ app.js
@@ -0,0 +1,25 @@
+const simpleGit = require('simple-git');
+
+const git = simpleGit('./');
+
+const actionInit = async () => {
+    const { files, current } = await git.status();
+
+    try {
+        if (files.length) {
+            await git.add('./*');
+
+            await git.commit(`feat: 分支${current}修改提交`);
+
+            await git.pull('origin', current);
+
+            await git.push('origin', current);
+            console.log('---log1---')
+            await git.checkout(['-b', 'rollback_dev', 'origin/main']);
+        }
+    } catch (err) {
+        console.log('err:', err);
+    }
+}
+
+actionInit();
